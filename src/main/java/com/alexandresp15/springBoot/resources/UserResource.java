6b987c2c0e9edb0b1bd8afbc9772308bac219f4e
Alexandre Pereira <alexandresp15@gmail.com>
2020-10-14 15:51:46 -0300

    User entity and resource

---
+++ src/main/java/com/alexandresp15/springBoot/resources/UserResource.java
@@ -0,0 +1,20 @@
+package com.alexandresp15.springBoot.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.alexandresp15.springBoot.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+
+	@GetMapping
+	public ResponseEntity<User> findAll() {
+		User user = new User(1L, "Maria", "maria@gmail.com", "122234442", "12345");
+		return ResponseEntity.ok().body(user);
+	}
+
+}
