3b1c8ac13465bfae468a32ac51c85ebf419d0d33
Alexandre Pereira <alexandresp15@gmail.com>
2020-10-14 16:30:41 -0300

    H2 database, test profile, JPA

--- src/main/java/com/alexandresp15/springBoot/entities/User.java
+++ src/main/java/com/alexandresp15/springBoot/entities/User.java
@@ -2,9 +2,17 @@ package com.alexandresp15.springBoot.entities;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class User implements Serializable{
 	private static final long serialVersionUID = 1L;
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
 	private String email;
@@ -62,7 +70,7 @@ public class User implements Serializable{
 	public void setPassword(String password) {
 		this.password = password;
 	}
-
+	
 	@Override
 	public int hashCode() {
 		final int prime = 31;
--- src/main/java/com/alexandresp15/springBoot/resources/UserResource.java
+++ src/main/java/com/alexandresp15/springBoot/resources/UserResource.java
@@ -14,7 +14,8 @@ public class UserResource {
 	@GetMapping
 	public ResponseEntity<User> findAll() {
 		User user = new User(1L, "Maria", "maria@gmail.com", "122234442", "12345");
-		return ResponseEntity.ok().body(user);
+		
+		return ResponseEntity.ok().body(user); 
 	}
 
 }
